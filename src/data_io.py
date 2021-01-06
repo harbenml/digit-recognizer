c67bcea0c5c6d0d6d11b13351ec4fc0423a64667
harbenml <harbenml@pm.me>
2021-01-06 17:25:44 +0100

    add dataloaders

---
+++ src/data_io.py
@@ -0,0 +1,42 @@
+from typing import IO, Tuple, cast
+
+from src.config import DATA_PATH, URL, FILENAME
+
+import gzip
+import numpy as np  # type: ignore
+import pickle
+import requests
+
+from torch.utils.data import TensorDataset
+from torch.utils.data import DataLoader
+
+
+def get_data() -> Tuple[TensorDataset, TensorDataset]:
+    """
+    Downloads the MNIST dataset from github, if not already done, and 
+    unzips the file. The function returns the training set and the 
+    validation set.
+    """
+    if not (DATA_PATH / FILENAME).exists():
+        content = requests.get(URL + FILENAME).content
+        (DATA_PATH / FILENAME).open("wb").write(content)
+
+    with gzip.open((DATA_PATH / FILENAME).as_posix(), "rb") as unzipped_file:
+        ((x_train, y_train), (x_valid, y_valid), _) = pickle.load(
+            cast(IO[bytes], unzipped_file), encoding="latin-1"
+        )
+
+    train_ds = TensorDataset(x_train, y_train)
+    valid_ds = TensorDataset(x_valid, y_valid)
+
+    return train_ds, valid_ds
+
+
+def get_dataloaders(
+    train_ds: TensorDataset, valid_ds: TensorDataset, bs: int
+) -> Tuple[DataLoader, DataLoader]:
+    return (
+        DataLoader(train_ds, batch_size=bs, shuffle=True),
+        DataLoader(valid_ds, batch_size=bs * 2),
+    )
+
