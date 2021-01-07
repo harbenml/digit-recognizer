54a4c1ec04fc713170e34abd339072d235ff0d4f
harbenml <harbenml@pm.me>
2021-01-07 19:34:19 +0100

    add train loop function

---
+++ src/train.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def fit(epochs, model, loss_func, opt, train_dl, valid_dl):
+    for epoch in range(epochs):
+        model.train()
+        for xb, yb in train_dl:
+            loss_batch(model, loss_func, xb, yb, opt)
+
+        model.eval()
+        with torch.no_grad():
+            losses, nums = zip(
+                *[loss_batch(model, loss_func, xb, yb) for xb, yb in valid_dl]
+            )
+        val_loss = np.sum(np.multiply(losses, nums)) / np.sum(nums)
+
+        print(epoch, val_loss)
