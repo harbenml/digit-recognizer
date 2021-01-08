740e05cf44d3ca6667f05aef1cf38229302fe198
harbenml <harbenml@pm.me>
2021-01-08 18:43:46 +0100

    add annotations to loss_batch

--- src/loss.py
+++ src/loss.py
@@ -1,18 +1,25 @@
-from typing import Iterable, Callable
+from typing import Any, Callable, Tuple
 
 from torch import nn
 from torch import optim
+from torch import Tensor
+
 from torch.utils.data import DataLoader
 import torch.nn.functional as F
 
 from src.model import Mnist_CNN
 
 loss_func = F.cross_entropy
+model = Mnist_CNN()
 
 
 def loss_batch(
-    model: Mnist_CNN = Mnist_CNN(), loss_func: Callable = loss_func, xb: Iterable, yb: Iterable, opt: optim = None
-):
+    xb: Tensor,
+    yb: Tensor,
+    opt: Any = None,
+    model: Callable = model,
+    loss_func: Callable = loss_func,
+) -> Tuple[Any, int]:
     loss = loss_func(model(xb), yb)
 
     if opt is not None:
