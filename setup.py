e52d74a78b89e605cd0e16af07b05c4e00e856dd
harbenml <harbenml@pm.me>
2021-01-12 17:44:33 +0100

    change setup.py with src

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 from setuptools import setup
 from setuptools import find_packages
 
-setup(
-    name="digit_recognizer", version="0.0.1", packages=find_packages(exclude=["tests"])
-)
+setup(name="digit_recognizer", version="0.0.1", packages=find_packages(where="src"))
